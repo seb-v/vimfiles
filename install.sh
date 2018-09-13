e079ac9b63ccceb25694cb044b7950248847970f
SebV <>
2018-09-13 10:50:11 +0200

    Fzf and add some config files

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+# Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# Package Installation
+sudo apt-get install python3
+
+#ctags
+sudo apt install exuberant-ctags
